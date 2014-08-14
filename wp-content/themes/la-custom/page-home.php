e8db4cdb3ff4dd57de83c5239ebbea854cbb538b
sethjgore <sethjgore@gmail.com>
2014-08-14 10:33:20 -0500

    added member_img to class

--- wp-content/themes/la-custom/page-home.php
+++ wp-content/themes/la-custom/page-home.php
@@ -97,50 +97,26 @@ get_header(); ?>
   <div class="clear"></div>
 </div>
 
-<div class="main slider_text texxt-center">
-      <h4>Affilated Organizations We Work With</h4>
+
+<div class="main_btm"><!-- start main_btm -->
+<div class="container">
+  <div class="main slider_text texxt-center">
+      <h4>Organizations We Work With</h4>
   </div>
 
-<div class="span_of_3"><!-- start span_of_4 -->
+  <div class="span_of_3"><!-- start span_of_4 -->
   <div class="col-md-3">
-    <img class="member_img" src="<?php bloginfo('template_url'); ?>/images/member_logo_2.png">
+    <img class="member_img center-block" src="<?php bloginfo('template_url'); ?>/images/member_logo_2.png">
   </div>
   <div class="col-md-3">
-    <img class="member_img" src="<?php bloginfo('template_url'); ?>/images/member_logo_3.png">
+    <img class="member_img center-block" src="<?php bloginfo('template_url'); ?>/images/member_logo_3.png">
   </div>
   <div class="col-md-3">
-    <img class="member_img" src="<?php bloginfo('template_url'); ?>/images/member_logo_1.png">
+    <img class="member_img center-block" src="<?php bloginfo('template_url'); ?>/images/member_logo_1.png">
   </div>
 
   <div class="clearfix"></div>
 </div><!-- end span_of_4 -->
-
-<div class="main_btm"><!-- start main_btm -->
-<div class="container">
-<div class="clear"></div>
-<hr>
-<div class="clear"></div>
-	<div class="cau_hide">
-	<div class="cursual"><!--  start cursual  he -->		<h4>Organizations we're a part of:<span class="line><"/span></h4>
-	</div>
-	<div id="owl-demo" class="owl-carousel"><!----start-img-cursual---->
-		<div class="item">
-			<div class="cau_left">
-				<img class="lazyOwl" data-src="<?php bloginfo('template_url'); ?>/images/member_logo_2.png" style="height:85px">
-			</div>
-		</div>
-    <div class="item">
-      <div class="cau_left">
-        <img class="lazyOwl" data-src="<?php bloginfo('template_url'); ?>/images/member_logo_1.png" style="height:85px">
-      </div>
-    </div>
-		<div class="item">
-			<div class="cau_left">
-				<img class="lazyOwl" data-src="<?php bloginfo('template_url'); ?>/images/member_logo_3.png" style="height:85px">
-			</div>
-		</div>
-	</div><!----//End-img-cursual---->
-	</div>
 </div>
 </div>
 
