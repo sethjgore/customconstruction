261245ac3895a6694806519aad206c1ccdb440a1
sethjgore <sethjgore@gmail.com>
2014-08-29 15:12:43 -0500

    --amend

--- wp-content/themes/la-custom/page-home.php
+++ wp-content/themes/la-custom/page-home.php
@@ -76,21 +76,33 @@ get_header(); ?>
   		<h4>Why LA Custom Construction?</h4>
   		<p>You have your choice of many home builders. Here are just a few examples of why our clients think we're the best.</p>
     </div>
-    <!-- start banner -->
-      <div class="row">
-        <div class="col-md-3"><div class="span4_of_list"><p>Luke & Jamie- Thank you for everything. We love our outdoor kitchen and have really enjoyed it this summer. Yall are extremely talented and we wish you the best of luck with your business</p><h3 class="is_color-salmon">Ken and Valerie</h3></div></div>
-        <div class="col-md-3"><div class="span4_of_list"><p>It was a pleasure working with you both in building my first new home. I was green behind the ears to the whole process but yall made it fun and easy. Best of luck on all your future endeavors</p><h3 class="is_color-salmon">Hannah</h3></div></div>
-        <div class="col-md-3"><div class="span4_of_list"><p>Thanks for a beautiful home!</p><h3 class="is_color-salmon">Johnelle & Rod</h3></div></div>
-    </div>
-    <div class="row">
-        <div class="col-md-3"><div class="span4_of_list"><p>I wanted to thank you all for being so organized and keeping in touch with me! That's so hard to fine in a contractor these days!</p><h3 class="is_color-salmon">Stacy</h3></div></div>
-        <div class="col-md-3"><div class="span4_of_list"> <p>Our new home is truly a work of art created by Luke and Jamie.  They took such pride in the creative touches and details.  They even made the purchasing process enjoyable!  It was much more than business - but a relationship that has been so fulfilling and satisfying </p><h3 class="is_color-salmon">Johnelle & Rod</h3></div></div>
-      </div>
 
-   <!-- end banner -->
+
+    <div class="main_grid">
+      <div class="span_of_4"><!-- start span_of_4 -->
+        <div class="col-md-4">
+          <div class="span4_of_list">
+           <p>Luke & Jamie- Thank you for everything. We love our outdoor kitchen and have really enjoyed it this summer. Yall are extremely talented and we wish you the best of luck with your business</p><h3 class="is_color-salmon">Ken and Valerie</h3>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="span4_of_list">
+
+           <p>It was a pleasure working with you both in building my first new home. I was green behind the ears to the whole process but yall made it fun and easy. Best of luck on all your future endeavors</p><h3 class="is_color-salmon">Hannah</h3>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="span4_of_list">
+            <p>I wanted to thank you all for being so organized and keeping in touch with me! That's so hard to fine in a contractor these days!</p><h3 class="is_color-salmon">Stacy</h3>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+      </div><!-- end span_of_4 -->
+    </div>
   </div>
-  <div class="clear"></div>
 </div>
+
+
 
 
 <div class="main_btm"><!-- start main_btm -->
